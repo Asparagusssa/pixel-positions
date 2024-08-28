b26ae4b139eb79e13fd2423e28f9f79bfda298d1
Asparagusssa <120052922+Asparagusssa@users.noreply.github.com>
2024-08-28 19:04:25 +0400

    make heading

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,20 @@
 <x-layout>
-    <section>
-        <x-section-heading>Top Jobs</x-section-heading>
-        <div class="grid lg:grid-cols-3 gap-8 mt-6">
-            <x-job-cart></x-job-cart>
-            <x-job-cart></x-job-cart>
-            <x-job-cart></x-job-cart>
-        </div>
-    </section>
+    <div class="space-y-10">
+        <section>
+            <x-section-heading>Top Jobs</x-section-heading>
+            <div class="grid lg:grid-cols-3 gap-8 mt-6">
+                <x-job-cart></x-job-cart>
+                <x-job-cart></x-job-cart>
+                <x-job-cart></x-job-cart>
+            </div>
+        </section>
+
+        <section>
+            <x-section-heading>Tags</x-section-heading>
+        </section>
+
+        <section>
+            <x-section-heading>Recent Jobs</x-section-heading>
+        </section>
+    </div>
 </x-layout>
