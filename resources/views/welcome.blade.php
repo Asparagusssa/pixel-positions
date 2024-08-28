7dd48e10f2188c6b22aab9b754f248398d1b1481
Asparagusssa <120052922+Asparagusssa@users.noreply.github.com>
2024-08-28 18:49:02 +0400

    add section-heading

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,8 +1,7 @@
 <x-layout>
     <section>
-        <h3 class="mb-6">Top Jobs</h3>
-
-        <div class="grid lg:grid-cols-3 gap-8">
+        <x-section-heading>Top Jobs</x-section-heading>
+        <div class="grid lg:grid-cols-3 gap-8 mt-6">
             <x-job-cart></x-job-cart>
             <x-job-cart></x-job-cart>
             <x-job-cart></x-job-cart>
