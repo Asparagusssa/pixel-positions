cd0aa5f5d5b836a885686442b0c08c7ecfa83d43
Asparagusssa <120052922+Asparagusssa@users.noreply.github.com>
2024-08-31 21:53:43 +0400

    complete html code

---
+++ resources/views/Components/job-card.blade.php
@@ -0,0 +1,17 @@
+<x-panel class="flex flex-col text-center">
+    <div class="self-start text-sm">Company</div>
+
+    <div class="px-8 pt-8 flex-1 flex flex-col">
+        <h3 class="group-hover:text-blue-800 text-xl mb-auto font-bold transition-colors duration-300">Full stack Laravel Developer</h3>
+        <p class="text-sm mt-4">Full time - From $60,000</p>
+    </div>
+    <div class="flex justify-between items-center mt-auto">
+        <div>
+            <x-tag size="small">Backend</x-tag>
+            <x-tag size="small">Frontend</x-tag>
+            <x-tag size="small">Manager</x-tag>
+        </div>
+
+        <x-employer-logo :width="42"/>
+    </div>
+</x-panel>
