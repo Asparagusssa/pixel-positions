a9230eb7796bbeaf73061acba02ffdb9e110edbc
Asparagusssa <120052922+Asparagusssa@users.noreply.github.com>
2024-09-01 18:23:03 +0400

    half-completed logic

--- resources/views/Components/job-card-wide.blade.php
+++ resources/views/Components/job-card-wide.blade.php
@@ -1,17 +1,19 @@
+@props(['job'])
 <x-panel class="flex gap-x-6">
     <div>
         <x-employer-logo />
     </div>
     <div class="flex-1 flex flex-col">
-        <a class="self-start text-sm text-gray-400">Company</a>
+        <a class="self-start text-sm text-gray-400">{{ $job->employer->name }}</a>
 
-        <h3 class="font-bold text-xl mt-3 group-hover:text-blue-800 transition-colors duration-300">Full stack Laravel Developer</h3>
-        <p class="text-sm text-gray-400 mt-auto">Full time - From $60,000</p>
+        <h3 class="font-bold text-xl mt-3 group-hover:text-blue-800 transition-colors duration-300">{{ $job->title }}</h3>
+        <p class="text-sm text-gray-400 mt-auto">{{ $job->schedule }} - From {{ $job->salary }}</p>
     </div>
     <div>
-        <x-tag>Tag</x-tag>
-        <x-tag>Tag</x-tag>
-        <x-tag>Tag</x-tag>
+        @foreach($job->tags as $tag)
+            <x-tag :$tag />
+        @endforeach
+
     </div>
 
 </x-panel>
--- resources/views/Components/job-card.blade.php
+++ resources/views/Components/job-card.blade.php
@@ -1,17 +1,21 @@
+@props(['job'])
+
 <x-panel class="flex flex-col text-center">
-    <div class="self-start text-sm">Company</div>
+    <a href="#">
+        <div class="self-start text-sm">{{ $job->employer->name }}</div>
 
-    <div class="px-8 pt-8 flex-1 flex flex-col">
-        <h3 class="group-hover:text-blue-800 text-xl mb-auto font-bold transition-colors duration-300">Full stack Laravel Developer</h3>
-        <p class="text-sm mt-4">Full time - From $60,000</p>
-    </div>
-    <div class="flex justify-between items-center mt-auto">
-        <div>
-            <x-tag size="small">Backend</x-tag>
-            <x-tag size="small">Frontend</x-tag>
-            <x-tag size="small">Manager</x-tag>
+        <div class="px-8 pt-8 flex-1 flex flex-col">
+            <h3 class="group-hover:text-blue-800 text-xl mb-auto font-bold transition-colors duration-300">{{ $job->title }}</h3>
+            <p class="text-sm mt-4">{{ $job->schedule }} - From {{ $job->salary }}</p>
         </div>
+        <div class="flex justify-between items-center mt-auto">
+            <div>
+                @foreach($job->tags as $tag)
+                    <x-tag :$tag size="small" />
+                @endforeach
+            </div>
 
-        <x-employer-logo :width="42"/>
-    </div>
+            <x-employer-logo :width="42"/>
+        </div>
+    </a>
 </x-panel>
